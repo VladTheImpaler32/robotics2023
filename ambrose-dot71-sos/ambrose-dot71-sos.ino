fcbacc9f2bc660aa3cc4f326bb40c2bbc1f73588
Ambrose <80496870+VladTheImpaler32@users.noreply.github.com>
2023-02-09 14:02:47 -0800

    Update ambrose-dot71-sos.ino

--- ambrose-dot71-sos/ambrose-dot71-sos.ino
+++ ambrose-dot71-sos/ambrose-dot71-sos.ino
@@ -1,16 +1,13 @@
 int sos[] = {1,1,1,2,2,2,1,1,1};
 void setup() {
-   pinMode(LED_BUILTIN, OUTPUT);
-}
+   pinMode(LED_BUILTIN, OUTPUT);}
 void loop() {
   for (int i=0; i<sizeof(sos)/sizeof(sos[0]); i++){
       digitalWrite(LED_BUILTIN, LOW);
       delay(sos[i]*500);
       digitalWrite(LED_BUILTIN, HIGH);           
-      delay(sos[i]*250);
-  }
-    delay(5000);
-}
+      delay(sos[i]*250);}
+    delay(5000);}
 
     // make the LED flash an SOS then wait 5 seconds and repeat
     // Who can do it in as few lines as possible!!
